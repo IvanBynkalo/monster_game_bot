298df7f94b48de0e920bfbbe25222e88f5568217
IvanBynkalo <104821715+IvanBynkalo@users.noreply.github.com>
2026-03-18 16:20:19 +0100

    Update board_menu.py

--- keyboards/board_menu.py
+++ keyboards/board_menu.py
@@ -1,17 +1,13 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-def board_menu(has_active_order: bool = False):
-    keyboard = []
-
-    if not has_active_order:
-        keyboard.append([KeyboardButton(text="📌 Взять заказ: Травник")])
-        keyboard.append([KeyboardButton(text="📌 Взять заказ: Руда")])
-
-    keyboard.append([KeyboardButton(text="📒 Мои заказы")])
-    keyboard.append([KeyboardButton(text="⬅️ Назад в город")])
-
+def board_menu():
     return ReplyKeyboardMarkup(
-        keyboard=keyboard,
+        keyboard=[
+            [KeyboardButton(text="📌 Взять заказ: Травник")],
+            [KeyboardButton(text="📌 Взять заказ: Руда")],
+            [KeyboardButton(text="📒 Мои заказы")],
+            [KeyboardButton(text="⬅️ Назад в город")],
+        ],
         resize_keyboard=True,
     )
